8f1f1e1d1d8130a1e94c326645106d662e83857b
Thomas Lebas <thomas.lebas26@gmail.com>
2023-09-02 15:56:54 +0200

    crash

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,9 +3,32 @@
 const { Router } = require('express');
 const router = new Router();
 
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+const User = require("../models/User.model");
+
 // GET route ==> to display the signup form to users
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
 // POST route ==> to process form data
+router.post('/signup', (req, res, next) => {
+    const { username, password } = req.body;
+   
+    bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        // username: username
+        username,
+        passwordHash: hashedPassword
+      });
+    })
+    .then(userFromDB => {
+      console.log('Newly created user is: ', userFromDB);
+    })
+    .catch(error => next(error));
+});
 
 module.exports = router;
