3c3c305fedf92025942240b7cd84d70fb3c7c508
Thomas Lebas <thomas.lebas26@gmail.com>
2023-09-02 16:04:42 +0200

    signup ok

--- models/User.model.js
+++ models/User.model.js
@@ -1,20 +1,17 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
-const { Schema, model } = require('mongoose');
- 
 const userSchema = new Schema(
   {
     username: {
       type: String,
       trim: true,
-      required: [true, 'Username is required.'],
+      required: [true, "Username is required."],
       unique: true
     },
-   
+    
     passwordHash: {
       type: String,
-      required: [true, 'Password is required.']
+      required: [true, "Password is required."]
     }
   },
   {
@@ -22,6 +19,4 @@ const userSchema = new Schema(
   }
 );
 
-const User = model("User", userSchema);
-
-module.exports = User;
+module.exports = model("User", userSchema);
